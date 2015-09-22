6d0e23cec7449f80cdebe6a0f0361046e5eda86f
bruno717 <bruno.oliveira@redspark.io>
2015-09-22 10:30:57 -0300

    toolbar and recyclerView

--- ExemploToolbar/app/src/main/java/com/example/brunooliveira/exemplotoolbar/MainActivity.java
+++ ExemploToolbar/app/src/main/java/com/example/brunooliveira/exemplotoolbar/MainActivity.java
@@ -1,16 +1,72 @@
 package com.example.brunooliveira.exemplotoolbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "LOG";
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.tb_main);
+        mToolbar.setTitle("Main Activity");
+        mToolbar.setSubtitle("just a subtitle");
+        mToolbar.setLogo(R.drawable.ic_launcher);
+        setSupportActionBar(mToolbar);
+
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                Intent intent;
+                intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+                return true;
+            }
+        });
+
+        mToolbarBottom = (Toolbar) findViewById(R.id.inc_tb_bottom);
+        mToolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+
+
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        Log.i("clique toolbar", "clicou!");
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        mToolbarBottom.inflateMenu(R.menu.menu_main);
+
+        mToolbarBottom.findViewById(R.id.iv_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Settings pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
